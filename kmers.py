b047b90230dbdf49e6e04f5761454043b02a7902
Ben <bjr.roberts@gmail.com>
2014-11-02 11:52:30 -0500

    Add files

---
+++ kmers.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+from collections import defaultdict, deque, Counter
+from itertools import islice
+
+def sliding_window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+def get_clumps(genome, k, L, t):
+    """
+    Given a genome, returns all (L,t)-clumps of k-mers.
+
+    See http://stackoverflow.com/a/26695030/2581969 for some explanation if that doesn't mean anything to you.
+    """
+    clumps = set()
+    kmers = KmerSequence(L-k)
+
+    # "prime" kmers with the first window
+    for kmer in sliding_window(genome[:L], k):
+        kmers.add(kmer)
+
+    # initial check for clumps
+    clumps |= kmers.above_t(t)
+
+    # now, run the actual algorithm
+    for window in sliding_window(genome[1:], L):
+        kmers.add(window[-k:])
+        clumps |= kmers.above_t(t)
+
+    return clumps
+
+class KmerSequence(object):
+    def __init__(self, limit):
+        self.order = deque()
+        self.counts = Counter()
+        self.bins = defaultdict(set)
+        self.limit = limit
+
+    def add(self, kmer):
+        if len(self.order) > self.limit:
+            self._remove_oldest()
+        self._add_one(kmer)
+
+    def _add_one(self,kmer):
+        self.order.append(kmer)
+        old_count = self.counts[kmer]
+        self.counts[kmer] = old_count + 1
+        if old_count > 0:
+            self.bins[old_count].remove(kmer)
+        self.bins[old_count+1].add(kmer)
+
+    def _remove_oldest(self):
+        toremove = self.order.popleft()
+        old_count = self.counts[toremove]
+        self.counts[toremove] -= 1
+        self.bins[old_count].remove(toremove)
+        if old_count > 1:
+            self.bins[old_count-1].add(toremove)
+
+    def above_t(self,t):
+        ret = set()
+        for b in (v for k,v in self.bins.items() if k >= t):
+            ret |= b
+        return ret
+
+if __name__ == '__main__':
+    import sys
+
+    with open(sys.argv[1]) as f:
+        genome = f.read()
+
+    if len(sys.argv) > 2:
+        #user specified k,L,t; use those
+        k,L,t = map(int, sys.argv[2:])
+    else:
+        #defaults
+        k = 9
+        L = 500
+        t = 3
+
+    clumps = get_clumps(genome, k,L,t)
+
+    print '({}, {})-clumps of {}-mers found in that file: {}'.format(
+        L,
+        t,
+        k,
+        len(clumps)
+        )
---
+++ test_kmers.py
@@ -0,0 +1,63 @@
+import unittest
+from kmers import sliding_window
+from kmers import get_clumps
+
+genome = 'ACTAGACATGAACATGAACATACTCACCAGACATACTACT'
+
+class TestKmerSequence(unittest.TestCase):
+    def test_15_3_clump(self):
+        k = 3
+        L = 15
+        t = 3
+
+        clumps = get_clumps(genome, k,L,t)
+
+        self.assertEqual(clumps, {tuple('ACA'), tuple('CAT')})
+
+    def test_t(self):
+        k = 3
+        L = 15
+
+        clumps_2 = {('A', 'C', 'A'),('T', 'G', 'A'),('A', 'A', 'C'),
+                    ('A', 'C', 'T'),('C', 'A', 'T'),('G', 'A', 'A'),
+                    ('A', 'T', 'G'),('T', 'A', 'C')}
+
+        self.assertEqual(get_clumps(genome, k,L,2), clumps_2)
+
+        clumps_3 = {tuple('ACA'), tuple('CAT')}
+        self.assertEqual(get_clumps(genome, k,L,3), clumps_3)
+
+        clumps_4 = set()
+        self.assertEqual(get_clumps(genome, k,L,4), clumps_4)
+
+    def test_k(self):
+        L = 15
+        t = 3
+
+        clumps_2 = {tuple('CA'), tuple('AT'), tuple('AC'), tuple('GA')}
+        self.assertEqual(get_clumps(genome, 2,L,t), clumps_2)
+
+        clumps_3 = {tuple('ACA'), tuple('CAT')}
+        self.assertEqual(get_clumps(genome, 3,L,t), clumps_3)
+
+        clumps_4 = set()
+        self.assertEqual(get_clumps(genome, 4,L,t), clumps_4)
+
+    def test_L(self):
+        k=3
+        t=3
+
+        clumps_10 = set()
+        self.assertEqual(get_clumps(genome, k,10,t), clumps_10)
+
+        clumps_15 = {tuple('ACA'), tuple('CAT')}
+        self.assertEqual(get_clumps(genome, k,15,t), clumps_15)
+
+        clumps_25 = {('A', 'C', 'A'),('C', 'A', 'T'),('T', 'A', 'C'),
+                    ('A', 'C', 'T')}
+
+        self.assertEqual(get_clumps(genome, k,25,t), clumps_25)
+        
+
+if __name__ == "__main__":
+    unittest.main()
